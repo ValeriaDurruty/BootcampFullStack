bcd9c9fb93c4208aecd7852e2468d5adb0b99716
Valeria Durruty <vale_gelp87@hotmail.com>
2024-07-12 16:56:28 -0300

    Se agrega validación de bootstrap, se modifica el manejo de contraste 1 y 2 para usar el atributo de bootstrap y se agrega validación de fecha de nacimiento

--- Formulario/index_script.js
+++ Formulario/index_script.js
@@ -1,29 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+          // aca invoco a las verificaciones, dentro del IF: si la primera pasa (checkValidity) se ejecuta la segunda (validacion)
+          if (!form.checkValidity() || !validacion()) {
+              event.preventDefault()
+              event.stopPropagation()
+          } else {
+                  event.preventDefault()
+                  document.getElementById("mensajegracias").style.display = "block"; // hacer visible el mensaje
+              }
+
+          form.classList.add('was-validated')
+      }, false)
+  })
+
+  // Cambio de hoja de estilo
   document.getElementById("contraste1").addEventListener("click", function() {
-    document.getElementById("hoja_estilo").setAttribute("href", "estilo.css");
+      document.getElementById("html").setAttribute("data-bs-theme", "light");
   });
 
   document.getElementById("contraste2").addEventListener("click", function() {
-    document.getElementById("hoja_estilo").setAttribute("href", "contraste.css");
+      document.getElementById("html").setAttribute("data-bs-theme", "dark");
   });
 });
 
 function validacion() {
   var email = document.getElementById("email").value;
   var fechaNac = document.getElementById("fechaNac").value;
-
-  // Validar que el email sea correcto
-  if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
-    alert("La dirección de email no es válida.");
-    return false;
-   }
-
-  // Validar que la fecha de nacimiento sea anterior a la fecha actual
+  //Variables para chequeo de fecha de nacimiento
   var fechaActual = new Date();
   var fechaNacimiento = new Date(fechaNac);
+  //Expresión regular para validar email
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  if (fechaNacimiento >= fechaActual) {
-    alert("La fecha de nacimiento debe ser anterior a la fecha actual.");
-    return false;
+  // Validar email
+  if (!emailRegex.test(email)) {
+      alert("La dirección de email no es válida.");
+      return false;
   }
-}
+
+  // Validar que la fecha de nacimiento sea anterior a la fecha actual
+  if (fechaNacimiento >= fechaActual) {
+      alert("La fecha de nacimiento debe ser anterior a la fecha actual.");
+      return false;
+  }
+  
+  return true;
+};
