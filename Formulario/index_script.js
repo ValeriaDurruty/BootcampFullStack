114f53f75d481a8e8e14dea4beea761631722ce5
Valeria Durruty <vale_gelp87@hotmail.com>
2024-07-11 23:08:32 -0300

    Agregado feature validación fecha de nacimiento

--- Formulario/index_script.js
+++ Formulario/index_script.js
@@ -10,9 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function validacion() {
   var email = document.getElementById("email").value;
+  var fechaNac = document.getElementById("fechaNac").value;
 
+  // Validar que el email sea correcto
   if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
     alert("La dirección de email no es válida.");
     return false;
    }
+
+  // Validar que la fecha de nacimiento sea anterior a la fecha actual
+  var fechaActual = new Date();
+  var fechaNacimiento = new Date(fechaNac);
+
+  if (fechaNacimiento >= fechaActual) {
+    alert("La fecha de nacimiento debe ser anterior a la fecha actual.");
+    return false;
+  }
 }
